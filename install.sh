f0b84d89fcc6a9933e5bcd32901eee48f59743e7
Rick Chang <chchang915@gmail.com>
2020-04-29 03:19:11 -0700

    install: make sure config is clean

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 BACKUP_DIR=backup_$(date +%F-%H%M%S)
 set -x
 mkdir $BACKUP_DIR
-cp -a ~/.vim $BACKUP_DIR/.vim
-cp -a ~/.vimrc $BACKUP_DIR/.vimrc
+mv ~/.vim $BACKUP_DIR/.vim
+mv ~/.vimrc $BACKUP_DIR/.vimrc
 
 cp -a .vimrc ~/.vimrc
 cp -a .vim ~/.vim
